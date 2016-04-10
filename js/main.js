c2a5694558a20055ccc8e310b5852c4520412726
Andrew-DiBella <adibella@ufl.edu>
2016-04-09 20:34:34 -0700

    add jquery and plugins

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$( document ).ready( function() {
+	$( 'h1' ).addClass( 'namechange' );
+} );
+
+//Nested jQuery plugin
+
+$( document ).ready( function() {
+
+} );
+
+//FlowType jQuery plugin
+
+$( document ).ready( function() {
+	$( 'body' ).flowtype( {
+		minimum: 500,
+		maximum: 800,
+		minFont: 12,
+		maxFont: 18,
+		fontRatio: 30
+	} );
+} );
+
+// jQuery .hover() trying to get working
+//$( 'h1' ).hover( function() {
+//	$( this ).addClass( 'namechage' ); },
+//
+//	function() {
+//		$( this ).removeClass( 'namechage' );
+//	} );
